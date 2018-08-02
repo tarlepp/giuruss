069d1d85d4902d72df18efcdf2495dc031ab4811
tarlepp <tarmo.leppanen@protacon.com>
2018-08-02 16:34:09 +0300

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\myFunctions\myMethods;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     *
+     * @param \App\myFunctions\myMethods $myMethods
+     *
+     * @return Response
+     */
+    public function index(myMethods $myMethods): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'foobar' => $myMethods->hello(),
+        ]);
+    }
+}
---
+++ src/myFunctions/myMethods.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\myFunctions;
+
+
+class myMethods
+{
+    public $sentence = 'hello world';
+
+    public function hello(): string
+    {
+        return $this->sentence;
+    }
+}
